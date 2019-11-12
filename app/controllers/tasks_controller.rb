2ae7453ee3d078c01d8524c27adac90cf8b06a0f
Etienne <etienne.burdet@gmail.com>
2019-11-12 15:16:52 +0100

    soucis de booleen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,36 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    title = params[:task][:title]
+    details = params[:task][:details]
+    @task = Task.new(title: title, details: details, completed: '0')
+    @task.save
+    redirect_to task_url(@task.id)
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_url
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_101306) do
+ActiveRecord::Schema.define(version: 2019_11_12_134333) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.string "completed"
+    t.string "completed", default: "f"
     t.string "boolean"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
